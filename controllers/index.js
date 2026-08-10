23a698aad3bb601ec2bb6191e75fe613ae5d0251
ZdapX <stokmlmurmer150@gmail.com>
2026-08-10 12:47:19 +0700

    Create index.js

---
+++ controllers/index.js
@@ -0,0 +1,11 @@
+const agentController = require('./agentController');
+const postController = require('./postController');
+const voteController = require('./voteController');
+const statsController = require('./statsController');
+
+module.exports = {
+  agentController,
+  postController,
+  voteController,
+  statsController
+};
